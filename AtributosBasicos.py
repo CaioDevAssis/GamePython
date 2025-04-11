36f5e81ce63f18b8aaa22816e6bce689df8abde8
CaioDevAssis <teachercaioassis@hotmail.com>
2025-04-11 07:28:50 -0300

    atribuots basicos criado

---
+++ AtributosBasicos.py
@@ -0,0 +1,8 @@
+
+
+class AtributosBasicos:
+    
+    def __init__(self, nivel, forca, inteligencia):
+        self.nivel = nivel
+        self.forca =forca
+        self.inteligencia = inteligencia
