4bcc5477f86009c1138edf6825db62bccfd3d654
CaioDevAssis <teachercaioassis@gmail.com>
2025-04-11 22:23:45 -0300

    alterações atributos basicos

--- AtributosBasicos.py
+++ AtributosBasicos.py
@@ -2,17 +2,27 @@ import math
 
 class AtributosBasicos:
     
-    def __init__(self, nivel, forca, inteligencia):
+    def __init__(self, nivel, forca, inteligencia, estamina, mana):
+        
         self.nivel = nivel
         self.forca =forca
         self.inteligencia = inteligencia
         self.base_player = 50
         
-        self.estaminaBase = self.base_player + int( math.sqrt(self.nivel) * self.nivel + math.sqrt(self.forca) * self.forca )      
+        self.estaminaBase = self.base_player + int( math.sqrt(self.nivel) * self.nivel + math.sqrt(self.forca) * self.forca ) 
+        if (estamina < self.estaminaBase):
+            self.estamina = estamina
+        else:
+            self.estamina = self.base_player
+        
         self.manaBase = self.base_player + int( math.sqrt(self.nivel) * self.nivel + math.sqrt(self.inteligencia) * self.inteligencia )
+        if (mana < self.manaBase):
+            self.mana = mana
+        else:
+            self.mana = self.base_player
         
         
-teste = AtributosBasicos(5,12,9)
+teste = AtributosBasicos(5,12,13, 100, 10)
 
-print(teste.estaminaBase)
-print(teste.manaBase)
+print(teste.estaminaBase,"/",teste.estamina)
+print(teste.manaBase,"/",teste.mana)
