6e88880c5c5c47b831d9a28ce510a6fdfab89fab
CaioDevAssis <teachercaioassis@hotmail.com>
2025-04-21 10:18:30 -0300

    atributos mdoficado

--- AtributosBasicos.py
+++ AtributosBasicos.py
@@ -9,15 +9,31 @@ class AtributosBasicos:
         self.inteligencia = inteligencia
         self.base_player = 50
         
-        self.estaminaBase = self.base_player + int( math.sqrt(self.nivel) * self.nivel + math.sqrt(self.forca) * self.forca ) 
+        self.setEstaminaBase()
         if (estamina < self.estaminaBase):
             self.estamina = estamina
         else:
             self.estamina = self.base_player
         
-        self.manaBase = self.base_player + int( math.sqrt(self.nivel) * self.nivel + math.sqrt(self.inteligencia) * self.inteligencia )
+        self.setManaBase()
         if (mana < self.manaBase):
             self.mana = mana
         else:
             self.mana = self.base_player
+            
+            
+    def setAtributos(self, valor):
+        return self.base_player + int( math.sqrt(self.nivel) * self.nivel + math.sqrt(valor) * valor )
+        
+    def setEstaminaBase(self):
+        self.estaminaBase = self.setAtributos(self.forca)
+        
+    def setManaBase(self):
+        self.manaBase = self.setAtributos(self.inteligencia)
+        
 
+
+teste = AtributosBasicos(1,12,10,30,30)
+
+print(teste.estaminaBase)
+print(teste.manaBase)
