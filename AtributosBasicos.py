d1a164e2b771500141535507db159a34be678f82
CaioDevAssis <teachercaioassis@hotmail.com>
2025-04-21 14:23:47 -0300

    teste adicionado

--- AtributosBasicos.py
+++ AtributosBasicos.py
@@ -29,42 +29,36 @@ class AtributosBasicos:
     def setEstaminaBase(self):
         self.estaminaBase = self.setAtributos(self.forca)
         
-    def setEstamina(self, estamina):        
-        if ( (self.estamina + estamina) < self.estaminaBase):
-            
-            if ( (self.estamina + estamina)> 0 ):            
+    def setEstamina(self, estamina): 
+        if (estamina >= 0):
+            if ( (self.estamina + estamina) < self.estaminaBase):
                 self.estamina += estamina
             else:
-                self.estamina = 0
+                self.estamina = self.estaminaBase
         else:
-            self.estamina = self.estaminaBase
+            if ( (self.estamina + estamina) >= 0):
+                 self.estamina += estamina
+            else:
+                self.estamina = 0
         
         
     def setManaBase(self):
         self.manaBase = self.setAtributos(self.inteligencia)
         
-    def setMana(self, mana):        
-        if ( (self.mana + mana) < self.manaBase):
-            
-            if ( (self.mana + mana)> 0 ):            
+    def setMana(self, mana): 
+        if (mana >= 0):
+            if ( (self.mana + mana) < self.manaBase):
                 self.mana += mana
             else:
-                self.mana = 0
+                self.mana = self.manaBase
         else:
-            self.mana = self.mana
+            if ( (self.mana + mana) >= 0):
+                 self.mana += mana
+            else:
+                self.mana = 0
         
         
     def Prints(self):
         print("\nNivel: ",self.nivel," Forca: ", self.forca, " Inteligencia: ", self.inteligencia)
         print("Estamina: ",self.estamina,"/",self.estaminaBase," -- Mana: ", self.mana,"/",self.manaBase)
         
-
-
-teste = AtributosBasicos(1,1,1,50,50)
-
-teste.Prints()
-
-teste.setEstamina(23)
-teste.setMana(-15)
-
-teste.Prints()
---
+++ teste.py
@@ -0,0 +1,10 @@
+import AtributosBasicos
+
+teste = AtributosBasicos.AtributosBasicos(1,1,1,50,50)
+
+teste.Prints()
+
+teste.setEstamina(23)
+teste.setMana(-15)
+
+teste.Prints()
